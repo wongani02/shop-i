99dc2bb851f6b4930d6a95c6897c2dcf3454e45a
wongani02 <81077944+wongani02@users.noreply.github.com>
2024-01-15 17:46:04 +0200

    update product category

--- src/app/(routes)/category/[categoryId]/page.tsx
+++ src/app/(routes)/category/[categoryId]/page.tsx
@@ -60,7 +60,7 @@ const CategorySinglePage: FunctionComponent<CategorySinglePageProps> = async ({p
                             {products.length === 0 && <Empty/>}
                             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                                 {products.map((product)=>(
-                                    <ProductCard product={product}/>
+                                    <ProductCard key={product.id} product={product}/>
                                 ))}
                             </div>
                         </div>
